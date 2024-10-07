24a88a1ec0bf7ba0553458d290459b75f387bc2b
Abhishek0712-ctrl <abhishekg9593@gmail.com>
2024-10-07 12:31:31 +0530

    Add project files.

---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*Console.WriteLine("Hello WOrld !");
+        int i = 10;
+        object o;
+        o = 21151;
+        Console.WriteLine(o);
+        */
+        //string s3 = "hello";
+        ////float f1 = Convert.ToSingle(s3);
+        //float.TryParse(s3, out float f1);
+        //Console.WriteLine(f1);
+        //Console.Read();
+        //if (args.Length > 0)
+        //{
+        //    foreach(string arg in args)
+        //    {
+        //        Console.WriteLine(arg);
+        //        Console.WriteLine("-------");
+
+        //        Console.WriteLine('Hello');
+        //    }
+        //}
+        //Console.Read();
+        string c = "A";
+        Console.WriteLine(c);
+        int _age = 11;
+        int _age<>10;
+        Console.WriteLine(_age);
+        Console.Read();
+        
+    }
+}
